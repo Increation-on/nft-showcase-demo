0bbe1ccf52bc20c258d2f8c369e4f52adffd13da
Maksim Dudarev <maxim.d132213@gmail.com>
2022-05-27 16:46:51 +0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { SectionWrapper, Download, Features } from "./components";
-import assets from './assets'
+import assets from './assets';
+import styles from "./styles/Global";
 const App = () => {
   return (
     <>
@@ -18,7 +19,27 @@ const App = () => {
         mockupImg={assets.homeCards}
         reverse
       />
-      <Features/>
+      <Features />
+      <SectionWrapper
+        title="Deployment"
+        description="ProNef is built using Expo which runs natively on all user's devices.
+        You can easily get your app into people's hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SectionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists of NFTs while the second one shows 
+        the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "}
+          <span className="bold">Pupa & Lupa</span>
+        </p>
+      </div>
     </>
   );
 }
